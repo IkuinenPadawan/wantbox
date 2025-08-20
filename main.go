53b456d14efaf68a82e4da1f93b63b7de8abc438
Andor <juuso.melentjeff@gmail.com>
2025-08-20 08:25:21 +0300

    feat: replace in-memory storage with SQLite integration

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	"log"
+	_ "modernc.org/sqlite"
 	"net/http"
 )
 
 type WishListItem struct {
 	ID       string  `json:"id"`
-	ItemName string  `json:"item"`
+	ItemName string  `json:"itemname"`
 	Url      string  `json:"url"`
 	Price    float64 `json:"price"`
+	UserId   int     `json:"user_id"`
 }
 
 type WishListPageData struct {
@@ -17,13 +21,71 @@ type WishListPageData struct {
 	WishlistItems []WishListItem
 }
 
-var wishlist = []WishListItem{
-	{ID: "1", ItemName: "Rudder Pedals", Url: "www.google.com", Price: 350.00},
-	{ID: "2", ItemName: "Synth", Url: "www.google.com", Price: 500.00},
-	{ID: "3", ItemName: "Saucony shoes", Url: "www.google.com", Price: 99.00},
+func findAll(db *sql.DB) ([]WishListItem, error) {
+	selectAll := `SELECT itemname, price, url FROM wishlist;`
+	rows, err := db.Query(selectAll)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var wishlistItems []WishListItem
+	for rows.Next() {
+		w := &WishListItem{}
+		err := rows.Scan(&w.ItemName, &w.Price, &w.Url)
+		if err != nil {
+			return nil, err
+		}
+		wishlistItems = append(wishlistItems, *w)
+	}
+	return wishlistItems, nil
 }
 
 func main() {
+	// DB
+	db, err := sql.Open("sqlite", "wantbox.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	createWishlistTableSql := `CREATE TABLE IF NOT EXISTS wishlist (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		itemname TEXT NOT NULL,
+		price REAL NOT NULL,
+		url TEXT NOT NULL,
+		user_id INTEGER NOT NULL,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+		);`
+
+	insertMockData := `INSERT INTO wishlist(itemname, price, url, user_id) VALUES("Rudder Pedals", 350.00, "www.google.com", 1);`
+	insertMockData2 := `INSERT INTO wishlist(itemname, price, url, user_id) VALUES("Synth", 500.00, "www.google.com", 1);`
+	insertMockData3 := `INSERT INTO wishlist(itemname, price, url, user_id) VALUES("Saucony shoes", 99.00, "www.google.com", 1);`
+
+	_, err = db.Exec(createWishlistTableSql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(insertMockData)
+	_, err = db.Exec(insertMockData2)
+	_, err = db.Exec(insertMockData3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	log.Println("Database connected successfully")
+
+	wishlist, err := findAll(db)
+	if err != nil {
+		log.Fatal("Error finding all items:", err)
+	}
+
+	// Router
 	router := gin.Default()
 	router.LoadHTMLFiles("layout.html")
 
